2a6eb14247400a571904f333adf019d4909c354b
DanielTTT <31566223+DanielTTT@users.noreply.github.com>
2017-09-12 12:37:47 +0900

    Update Depict.js

--- Depict.js
+++ Depict.js
@@ -1,5 +1,6 @@
-Depict=(function(){
 
+
+Depict=(function(){
 
   Object.prototype.Canvas=function(prop){
 
@@ -9,14 +10,39 @@ Depict=(function(){
     var ctx=canvas.getContext("2d");
     var self=this;
 
-    canvas.width=prop.width;
-    canvas.height=prop.height;
-    canvas.style.border=prop.border+"px solid";
-    canvasContainer.align="center"
+    if(prop.width==="maximum"){
+      //regulate the size of the canvas maximum
+      window.addEventListener("load",defineW);
+      window.addEventListener("resize",defineW);
+    }else{
+      canvas.width=prop.width;
+    }
+
+    if(prop.height==="maximum"){
+      //regulate the size of the canvas maximum
+      window.addEventListener("load",defineH);
+      window.addEventListener("resize",defineH);
+    }else{
+      canvas.height=prop.height;
+    }
+
+    function defineW(){  canvas.width=window.innerWidth;  }
+
+    function defineH(){  canvas.height=window.innerHeight; }
+
+
     canvasContainer.width=canvas.width;
     canvasContainer.height=canvas.height;
+    canvas.style.border=prop.border+"px solid";
+    canvasContainer.align="center"
     body.appendChild(canvasContainer);
     canvasContainer.appendChild(canvas);
+
+    Object.defineProperty(this,"prop",{
+      writable:true,
+      value:prop
+    })
+
 
     Object.defineProperty(this,"canvas",{
       writable:true,
@@ -53,8 +79,6 @@ Depict=(function(){
       writable:true,
       value:prop
     })
-
-
 
   }
 
@@ -93,6 +117,33 @@ Depict=(function(){
     })
 
   }
+
+  Object.prototype.Image=function(prop){
+
+    Object.defineProperty(this,"prop",{
+      writable:true,
+      value:prop
+    })
+
+
+  }
+
+  Object.prototype.Image.prototype.draw=function(canvas){
+
+    if(!this.canvas)this.canvas=canvas;
+    if(canvas)canvas.setter=this;
+    var ctx=this.canvas.canvas.getContext("2d");
+    var prop=this.prop
+    var img=new Image();
+    img.src=prop.src;
+
+    ctx.beginPath();
+    ctx.drawImage(img,prop.x-prop.width/2,prop.y-prop.height/2,prop.width,prop.height);
+    ctx.globalAlpha=prop.transprancy;
+    ctx.closePath();
+  }
+
+
 
   Object.prototype.Text.prototype.draw=function(canvas){
 
@@ -137,7 +188,6 @@ Depict=(function(){
     ctx.moveTo(prop.x[0],prop.y[0]);
     ctx.lineTo(prop.x[1],prop.y[1]);
     ctx.stroke();
-
   }
 
   Object.prototype.Circle.prototype.draw=function(canvas){
@@ -186,20 +236,49 @@ Depict=(function(){
 }
 
 
-var objectgroup=[Line,Rectangle,Circle,Text]
-
-for(var i in objectgroup){
 
 
-  if(objectgroup[i]!=Line){
     //for Rectangle,Circle,Text
-  objectgroup[i].prototype.migrate=function(x,y,speed,fn){
+  Object.prototype.migrate=function(x,y,speed,fn){
     var prop=this.prop;
     var self=this;
+
+    if(typeof prop.x==="object"){
+      console.log("A");
+      if(!lengthX0){
+        var lengthY0=y[0]-prop.y[0];
+        var lengthX0=x[0]-prop.x[0];
+        var lengthY1=y[1]-prop.y[1];
+        var lengthX1=x[1]-prop.x[1];
+
+      }
+
+      var interval=setInterval(function(){
+
+        prop.x[0]+=lengthX0/speed*10;
+        prop.y[0]+=lengthY0/speed*10;
+        prop.x[1]+=lengthX1/speed*10;
+        prop.y[1]+=lengthY1/speed*10;
+
+        if(Math.abs(x[0]-prop.x[0])<1 && Math.abs(y[0]-prop.y[0])<1){
+          Math.ceil(prop.y[0]);
+          Math.ceil(prop.x[0]);
+          if(typeof fn==="function" &&
+        self.canvas.instances.includes(self)){
+            fn();
+          }
+
+          clearInterval(interval);
+
+        }
+
+      },1)
+    }else{
+
     if(!lengthX){
       var lengthY=y-prop.y;
       var lengthX=x-prop.x;
-  }
+    }
 
     var interval=setInterval(function(){
 
@@ -220,46 +299,16 @@ for(var i in objectgroup){
 
     },1)
   }
+}
 
-}else{
-  //for Line
-
-  objectgroup[i].prototype.migrate=function(x,y,speed,fn){
-    var prop=this.prop;
-
-    if(!lengthX){
-      var lengthX=x-prop.x[0];
-      var lengthY=y-prop.y[0];
-    }
-
-    var interval=setInterval(function(){
-
-      for(var i in prop.x){
-
-        prop.x[i]+=lengthX/speed*10;
-        prop.y[i]+=lengthY/speed*10;
-
-      }
-
-      if(Math.abs(x-prop.x[0])<1 && Math.abs(y-prop.y[0])<1){
-
-        fn();
-        clearInterval(interval);
-        }
-
-      },1)
-
-    }
-  }
-
-  objectgroup[i].prototype.remove=function(canvas){
+  Object.prototype.remove=function(canvas){
     //clears all objects drawn in the canvas
     canvas.instances.splice(canvas.instances.indexOf(this),1);
 
   }
 
 
-  objectgroup[i].prototype.fadeIn=function(canvas,speed){
+  Object.prototype.fadeIn=function(canvas,speed){
 
       var prop=this.prop;
       this.prop.transprancy=0;
@@ -280,7 +329,7 @@ for(var i in objectgroup){
 
   }
 
-  objectgroup[i].prototype.fadeOut=function(canvas,speed){
+  Object.prototype.fadeOut=function(canvas,speed){
 
     var prop=this.prop;
     var self=this;
@@ -303,7 +352,7 @@ for(var i in objectgroup){
   },1)
   }
 
-  objectgroup[i].prototype.observeCollision=function(object2,fn){
+  Object.prototype.observeCollision=function(object2,fn){
 
     var self=this;
     var prop1=this.prop;
@@ -344,7 +393,6 @@ for(var i in objectgroup){
         return prop2.y;
       }
     }
-
 
 
     interval1()
@@ -391,7 +439,7 @@ for(var i in objectgroup){
       }
     }
 
-    objectgroup[i].prototype.scale=function(w,h,speed){
+    Object.prototype.scale=function(w,h,speed){
 
       var self=this;
       if(!this.prop.fontSize){
@@ -446,7 +494,7 @@ for(var i in objectgroup){
           }
     }
 
-    objectgroup[i].prototype.onclick=function(fn){
+    Object.prototype.onclick=function(fn){
 
       var canvas=this.canvas.canvas;
       var self=this;
@@ -460,7 +508,7 @@ for(var i in objectgroup){
       })
     }
 
-    objectgroup[i].prototype.mouseover=function(fn){
+    Object.prototype.mouseover=function(fn){
 
       var canvas=this.canvas.canvas;
       var self=this;
@@ -474,7 +522,7 @@ for(var i in objectgroup){
       })
     }
 
-    objectgroup[i].prototype.mouseout=function(fn){
+    Object.prototype.mouseout=function(fn){
 
       var canvas=this.canvas.canvas;
       var self=this;
@@ -486,9 +534,7 @@ for(var i in objectgroup){
         observeCollision2(e,fn,self)
       }
       })
-    }
 }
-
 
   function _CreateElement(element,prop){
 
